46eba89b2da09df4fbab0efd3803da26d20850af
Moshe Harush <moshe@haru.sh>
2024-05-15 21:19:04 +0300

    🎉 init

---
+++ apps/server/src/index.ts
@@ -0,0 +1,8 @@
+import libcjs from '@mono/lib-cjs';
+import libesm from '@mono/lib-module';
+import { print } from './utils';
+
+
+print(libcjs.name);
+
+print(libesm.name);
---
+++ apps/server/src/utils.ts
@@ -0,0 +1,3 @@
+export function print(str: string = "Hello World") {
+  console.log('str is: ', str)
+}
